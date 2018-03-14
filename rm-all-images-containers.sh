645cae530bce008d4a9cd3697e3a3c1c563d0c2b
Tomek Grzechowski <keo@becomewater.com>
2018-03-14 17:40:17 +0100

    fixing clean up networks

--- rm-all-images-containers.sh
+++ rm-all-images-containers.sh
@@ -8,6 +8,9 @@ echo
 
 sleep 15
 
-
+echo "REMOVING ALL CONTAINERS"
 for i in `docker ps -a | awk '{print $1}' | grep -v CONT`; do docker rm $i; done
+echo "REMOVING ALL IMAGES"
 for i in `docker images | awk '{print $3}' | grep -v IMAG`; do docker rmi -f $i; done
+echo "REMOVING ALL NETWORKS"
+for i in `docker network ls | grep default | cut -d' ' -f1`; do docker network rm $i; done
